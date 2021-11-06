debe27097312d16823ec41dea3ea10c6811f42e0
Willames <wyllamesro@gmail.com>
2021-11-06 10:40:18 -0300

    modificanto configurações de acesso a api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://192.168.0.150:3333'
-    // baseURL: 'http://10.0.3.7:3333'
+    // baseURL: 'http://192.168.0.150:3333'
+    baseURL: 'http://192.168.3.4:3333'
 })
 
 export  {api}
