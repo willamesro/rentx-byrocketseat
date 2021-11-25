aa1e036d78696e7b973f7f19b511ef67211b525f
Willames <wyllamesro@gmail.com>
2021-11-25 19:11:27 -0300

    screen Profile

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,44 +1,135 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard } from 'react-native'
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
 import { useNavigation } from '@react-navigation/native';
 import { Feather } from '@expo/vector-icons'
-import { HeaderTitle } from '@react-navigation/stack';
 import { useTheme } from 'styled-components';
 import { BackButton } from '../../components/BackButton';
 
-import { Container, Header, HeaderTop, LogoutButton, Photo, PhotoContainer } from './styles'
+import {
+    Container,
+    Header,
+    HeaderTop,
+    HeaderTitle,
+    LogoutButton,
+    PhotoContainer,
+    Photo,
+    PhotoButton,
+    Content,
+    Options,
+    Option,
+    OptionTitle,
+    Section
+} from './styles'
+import { Input } from '../../components/Input';
+import { InputPassword } from '../../components/InputPassword';
 
 export function Profile() {
+    const [option, setOption] = useState<'dataEdit' | 'passwordEdit'>('dataEdit')
     const theme = useTheme()
     const navigation = useNavigation()
 
     function handleSignOut() {
 
     }
+    function handleOnChange(optionSelected: 'dataEdit' | 'passwordEdit') {
+        setOption(optionSelected)
+    }
 
     function handleBack() {
         navigation.goBack()
     }
     return (
-        <Container>
-            <Header>
-                <HeaderTop>
-                    <BackButton
-                        color={theme.colors.shape}
-                        onPress={handleBack}
-                    />
-                    <HeaderTitle>Editar perfil</HeaderTitle>
-                    <LogoutButton onPress={handleSignOut}>
-                        <Feather
-                            name='power'
-                            size={24}
-                            color={theme.colors.shape}
-                        />
-                    </LogoutButton>
-                </HeaderTop>
-                <PhotoContainer>
-                    <Photo source={{ uri:'https://avatars.githubusercontent.com/u/58307770?v=4'}} />
-                </PhotoContainer>
-            </Header>
-        </Container>
+        <KeyboardAvoidingView behavior='position' enabled >
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss} >
+
+
+                <Container>
+                    <Header>
+                        <HeaderTop>
+                            <BackButton
+                                color={theme.colors.shape}
+                                onPress={handleBack}
+                            />
+                            <HeaderTitle>Editar perfil</HeaderTitle>
+                            <LogoutButton onPress={handleSignOut}>
+                                <Feather
+                                    name='power'
+                                    size={24}
+                                    color={theme.colors.shape}
+                                />
+                            </LogoutButton>
+                        </HeaderTop>
+                        <PhotoContainer>
+                            <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/58307770?v=4' }} />
+                            <PhotoButton>
+                                <Feather
+                                    name='camera'
+                                    size={24}
+                                    color={theme.colors.shape}
+                                />
+                            </PhotoButton>
+                        </PhotoContainer>
+                    </Header>
+                    <Content style={{ marginBottom: useBottomTabBarHeight() }}>
+                        <Options>
+                            <Option
+                                active={option === 'dataEdit'}
+                                onPress={() => handleOnChange('dataEdit')}
+                            >
+                                <OptionTitle active={option === 'dataEdit'}>
+                                    Dados
+                                </OptionTitle>
+                            </Option>
+                            <Option
+                                active={option === 'passwordEdit'}
+                                onPress={() => handleOnChange('passwordEdit')}
+                            >
+                                <OptionTitle active={option === 'passwordEdit'}>
+                                    Trocar senha
+                                </OptionTitle>
+                            </Option>
+                        </Options>
+                        {option === 'dataEdit' ?
+                            <Section >
+                                <Input
+                                    iconeName='mail'
+                                    editable={false}
+                                />
+                                <Input
+                                    iconeName='user'
+                                    placeholder='Nome'
+                                    autoCorrect={false}
+                                />
+                                <Input
+                                    iconeName='credit-card'
+                                    placeholder='CNH'
+                                    keyboardType='numeric'
+                                />
+                            </Section>
+                            :
+                            <Section >
+                                <InputPassword
+                                    iconeName='lock'
+                                    placeholder='Senha atual'
+                                />
+                                <InputPassword
+                                    iconeName='lock'
+                                    placeholder='Nova senha'
+                                />
+                                <InputPassword
+                                    iconeName='lock'
+                                    placeholder='Repita a nova senha'
+                                />
+
+
+                            </Section>
+                        }
+
+                    </Content>
+                </Container>
+            </TouchableWithoutFeedback>
+
+        </KeyboardAvoidingView>
     )
 }
--- src/screens/Profile/styles.ts
+++ src/screens/Profile/styles.ts
@@ -1,16 +1,100 @@
 import styled from 'styled-components/native'
-import { BorderlessButton } from 'react-native-gesture-handler'
+import { BorderlessButton, RectButton } from 'react-native-gesture-handler'
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { RFValue } from 'react-native-responsive-fontsize';
 
-export const Container = styled.View``;
+interface OptionsProps {
+    active: boolean
+}
+export const Container = styled.View`
+    background-color: ${({ theme }) => theme.colors.background_primary};
+`;
 
-export const Header = styled.View``;
+export const Header = styled.View`
+    background-color: ${({ theme }) => theme.colors.header};
+    
+    width:100%;
+    height: 227px;
+    padding: 0 24px;
+    align-items: center;
 
-export const HeaderTop = styled.View``;
+`;
 
-export const HeaderTitle = styled.Text``;
+export const HeaderTop = styled.View`
+    width: 100%;
+    flex-direction: row;
+    margin-top: ${getStatusBarHeight() + 30}px;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+export const HeaderTitle = styled.Text`
+    font-family: ${({ theme }) => theme.fonts.secondary_600};
+    font-size: ${RFValue(25)}px;
+
+    color: ${({ theme }) => theme.colors.shape};
+`;
 
 export const LogoutButton = styled(BorderlessButton)``;
 
-export const PhotoContainer = styled.View``;
+export const PhotoContainer = styled.View`
+    width:180px; 
+    height:180px;
+    border-radius: 90px;
+    margin-top: 48px;
+    background-color: ${({ theme }) => theme.colors.background_primary};
+    align-items: center;
+    justify-content: center;
+`;
 
-export const Photo = styled.Image``;
+export const Photo = styled.Image`
+    width:174px; 
+    height:174px;
+    border-radius: 87px;
+`;
+export const PhotoButton = styled(RectButton)`
+    position:absolute;
+    right: 10px;
+    bottom:10px;
+    width:40px;
+    height:40px;
+    align-items: center;
+    justify-content: center;
+    background-color: ${({ theme }) => theme.colors.main};
+
+`;
+
+export const Content = styled.View`
+    width: 100%;
+   
+    margin-top: 122px;
+    border-bottom-width: 1px;
+    padding: 0 24px;
+    border-bottom-color: ${({ theme }) => theme.colors.line}  
+`;
+
+export const Options = styled.View`
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+    /* margin-left: 52px; */
+    padding: 0 32px;
+    margin-bottom: 24px;
+
+`;
+export const Option = styled.TouchableOpacity<OptionsProps>`
+    padding-bottom:14px ;
+    border-bottom-width: ${({ active }) => active ? 2 : 0}px;
+    border-bottom-color: ${({ theme }) => theme.colors.main};
+
+`;
+
+export const OptionTitle = styled.Text<OptionsProps>`
+    font-family: ${({ theme, active }) => active ? theme.fonts.secondary_600 : theme.fonts.secondary_400};
+    color: ${({ theme, active }) => active ? theme.colors.title : theme.colors.shape};
+    font-size: ${RFValue(20)}px;
+`;
+
+export const Section = styled.View`
+    
+`;
