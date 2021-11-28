63f9f3c90cad474a27973bbafa9dda3510941140
Willames <wyllamesro@gmail.com>
2021-11-27 23:56:24 -0300

    deploying database synchronization function

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -59,9 +59,9 @@ export function Home() {
                 const carCollection = database.get<ModelCar>('cars')
                 const cars = await carCollection.query().fetch()
 
-                // const response = await api.get('/cars')
+                const response = await api.get('/cars')
 
-                // if (isMouted) setCars(response.data)
+                if (isMouted) setCars(response.data)
 
             } catch (error) {
                 console.log(error)
